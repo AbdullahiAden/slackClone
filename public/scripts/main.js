e9135b3af910e2488e494b49379d841d62a7f0d8
Abdullahi Aden <Abbe6152@gmail.com>
2021-03-12 21:08:29 +0100

    mesaging workin, saving and getting messages

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,46 +2,41 @@ let chatform = document.getElementById("form");
 let input = document.getElementById("msg");
 let chatMessages = document.getElementsByClassName("chat-messages");
 
-
 // get the channels id from the the coversation header of the message field
 let channelId = document.getElementById("channelId").textContent;
 
 const socket = io();
 // * fetched messages........
-socket.on("outputmsg", (messages ) => {
-    console.log(messages);
-    
+socket.on("outputmsg", (messages) => {
+  console.log(messages);
   //  loop through the channels in our db, match channelId from message header(which is hidden)
-// get the appropriate channel and its messages and output to the dom 
+  // get the appropriate channel and its messages and output to the dom
 
   for (let message in messages) {
     let msgIds = messages[message]._id;
     let currentChannelMsgs = messages[message].conversation;
     // console.log(msgIds);
 
-    if(channelId === msgIds){
-        console.log(currentChannelMsgs);
-        for(currentChannelMsg of currentChannelMsgs){
-           let currentmessages= currentChannelMsg.message
-            console.log(currentmessages);
+    if (channelId === msgIds) {
+      console.log(currentChannelMsgs);
+      for (currentChannelMsg of currentChannelMsgs) {
+        let currentmessages = currentChannelMsg.message;
+        console.log(currentmessages);
 
-            outputMessage(currentmessages);
-        }
+        outputMessage(currentmessages);
+      }
     }
-    
   }
-
- 
-
 });
 
 //* message from server
 // socket.emit("message", { channel: channelId, message: msg });
 socket.on("message", (message) => {
-// socket.on("message", ({ channel: channelId, message: message }) => {
+  // socket.on("message", ({ channel: channelId, message: message }) => {
   console.log(message + "..........");
-  // call func on this message to add to dom
-  outputMessage(message);
+  // call func on this message to add to dom -- emit the message, message.message will emit the content not the object
+
+  outputMessage(message.message);
 });
 
 // message submit
@@ -51,7 +46,7 @@ chatform.addEventListener("submit", (e) => {
   let msg = e.target.elements.msg.value;
 
   //  emit message to server
-//send an object to the server, the channel and the message that is typed
+  //send an object to the server, the channel and the message that is typed
   socket.emit("chatMessage", { channel: channelId, message: msg });
   // Clear input
   e.target.elements.msg.value = "";
@@ -70,13 +65,13 @@ function outputMessage(message) {
   document.querySelector(".chat-messages").appendChild(div);
 }
 
-function outputMessage(outputmsg) {
+function outputMessage(currentmessages) {
   const div = document.createElement("div");
   div.classList.add("message");
 
   div.innerHTML = `<p class="meta "> abbe <span>9.10pm</span></p>
     <p class="text">
-        ${outputmsg}
+        ${currentmessages}
     </p>`;
   document.querySelector(".chat-messages").appendChild(div);
 }
