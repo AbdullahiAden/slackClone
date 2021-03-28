7efee42c86a6eefc79c058ec61553dcdb3963053
Abdullahi Aden <Abbe6152@gmail.com>
2021-03-28 13:51:26 +0200

    DONE . admin deletion, logged in user deletion.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,45 +4,35 @@ let input = document.getElementById("msg");
 // get the channels id from the the coversation header of the message field
 let channelId = document.getElementById("channelId").textContent;
 
+let userId = document.getElementById("userId").textContent;
 
-let userId = document.getElementById('userId').textContent;
+let loggedInUserProfilePic = document.querySelector(".loggedInUserProfilePic")
+  .src;
 
-let loggedInUserProfilePic = document.querySelector('.loggedInUserProfilePic').src;
-
-
-let userName = document.getElementById('userName').textContent;
+let userName = document.getElementById("userName").textContent;
 
 const socket = io();
 
-socket.emit('login',{userId:userId});
+socket.emit("login", { userId: userId });
 socket.on("onlineUsers", (onlineUsers) => {
-  
   console.log(onlineUsers);
-  onlineUsers.forEach(onlineUser => {
-    renderOnlineUsers(onlineUser)
+  onlineUsers.forEach((onlineUser) => {
+    renderOnlineUsers(onlineUser);
   });
+});
 
-  
-  
-})
-
-function renderOnlineUsers(onlineUsers){
+function renderOnlineUsers(onlineUsers) {
   const onlineUsersDiv = document.createElement("div");
   const onlineUsersNames = document.createElement("div");
-  onlineUsersDiv.classList.add("onlineUsers")
-    
- 
+  onlineUsersDiv.classList.add("onlineUsers");
+
   // onlineUsersNames.innerHTML = `<p class="msgUser ">  ${userName}  </p> `
-  
+
   onlineUsersDiv.innerHTML = `<p > ${onlineUsers} </p> `;
   // onlineUsersDiv.append(onlineUsersNames)
   //   document.querySelector(".chat-messages").appendChild(eachDmDiv);
-    document.querySelector(".onlineUsers").appendChild(onlineUsersDiv);
-
-
+  document.querySelector(".onlineUsers").appendChild(onlineUsersDiv);
 }
-
-
 
 //  fetched messages........
 socket.on("outputmsg", (allChannels) => {
@@ -51,51 +41,47 @@ socket.on("outputmsg", (allChannels) => {
   //  loop through the channels in our db, match channelId from messagefields header(which is hidden)
   // get the appropriate channel and its messages and output to the dom
   for (let currentChannel in allChannels) {
-
     let channnelIds = allChannels[currentChannel]._id;
-    let fullCurrentChannel=allChannels[currentChannel]
-    
-    
+    let fullCurrentChannel = allChannels[currentChannel];
     // console.log(fullCurrentChannel );
     let currentChannelMsgs = allChannels[currentChannel].conversation;
-    console.log(currentChannelMsgs);
 
     if (channelId === channnelIds) {
-      // if the logged in user IS admin in that channel, he/ she will get the delete functionality for the messages 
-
-      if(fullCurrentChannel.admin===userId){
-        // console.log(fullCurrentChannel.admin);
-        // console.log("admin");
-
-        // console.log(currentChannelMsgs);
+      // if the logged in user IS admin in that channel, he/ she will get the delete functionality for the messages
+      // * give the delete to also the messages user if he/she is logged in
       for (currentChannelMsg of currentChannelMsgs) {
-        // all messages in the  databasein for the current channel , send to function to be outputted 
-        let currentmessages = currentChannelMsg.message;
-        // console.log(currentmessages);
-      
-        outputMessagesForAdmin(currentChannelMsg);
-      }
-      }
-      // if the logged in user IS NOT admin in that channel, he/ she will not get the delete functionality for the messages 
-      else if(fullCurrentChannel.admin !==userId){
-        for (currentChannelMsg of currentChannelMsgs) {
-          // all messages in the  databasein for the current channel , send to function to be outputted 
+        console.log(currentChannelMsg.user._id);
+        if (fullCurrentChannel.admin === userId ||currentChannelMsg.user._id===userId ) {
+          // all messages in the  databasein for the current channel , send to function to be outputted
           let currentmessages = currentChannelMsg.message;
-        
-          outputMessage(currentChannelMsg);
+
+          if (currentChannelMsg.user._id === userId) {
+            console.log("logged in users messages");
+
+            console.log(currentChannelMsg.message);
+          }
+
+          // console.log(currentmessages);
+          outputMessagesForAdmin(currentChannelMsg);
         }
 
+        // if the logged in user IS NOT admin in that channel, he/ she will not get the delete functionality for the messages
+        else if (fullCurrentChannel.admin !== userId ||currentChannelMsg.user._id !==userId) {
+          for (currentChannelMsg of currentChannelMsgs) {
+            // all messages in the  databasein for the current channel , send to function to be outputted
+            let currentmessages = currentChannelMsg.message;
 
+            outputMessage(currentChannelMsg);
+          }
+        }
       }
-      
     }
   }
-  
 });
 
 // message from server
-socket.on("message", (message ) => {
-  console.log(message) ;
+socket.on("message", (message) => {
+  console.log(message);
 
   // * call func on this message to add typed message to to dom , BUT CANNOT GET THE LOGGED IN USES PROFILE PIC AND NAME, gets only after reload
   outputMessage(message);
@@ -109,14 +95,18 @@ chatform.addEventListener("submit", (e) => {
 
   //  emit message to server
   //  send an object to the server, the channel and the message that is typed
-  socket.emit("chatMessage", { channel: channelId, message: msg , user : userId});
+  socket.emit("chatMessage", {
+    channel: channelId,
+    message: msg,
+    user: userId,
+  });
 
   // Clear input
   e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
-// output db message to dom- this 
+// output db message to dom- this
 function outputMessagesForAdmin(currentChannelMsg) {
   const div = document.createElement("div");
   const msgTextdiv = document.createElement("div");
@@ -125,24 +115,22 @@ function outputMessagesForAdmin(currentChannelMsg) {
   msgTextdiv.classList.add("messageBlock");
   eachMessageDiv.classList.add("eachMessageDiv");
 
-  if(!currentChannelMsg.user){
-  `<p class="msgText"> ${currentChannelMsg.message}</p> `
+  if (!currentChannelMsg.user) {
+    `<p class="msgText"> ${currentChannelMsg.message}</p> `;
+  } else {
+    div.innerHTML = `<img class="avatar" src="../uploads/${currentChannelMsg.user.profilePic}"></img>`;
 
-  }else{
-    
-  div.innerHTML = `<img class="avatar" src="../uploads/${currentChannelMsg.user.profilePic}"></img>`;
-    
-  msgTextdiv.innerHTML = `<p class="msgUser ">  ${currentChannelMsg.user.name}  <span class= "msgDate">${currentChannelMsg.timestamp} </span></p>
+    msgTextdiv.innerHTML = `<p class="msgUser ">  ${currentChannelMsg.user.name}  <span class= "msgDate">${currentChannelMsg.timestamp} </span></p>
 
 
     <p class="msgText"> ${currentChannelMsg.message}</p>
     <a href="/channels/${currentChannelMsg._id}/delete">delete</a>
     `;
 
-    eachMessageDiv.append(div)
-    eachMessageDiv.append(msgTextdiv)
-  document.querySelector(".chat-messages").appendChild(eachMessageDiv);
-  // document.querySelector(".chat-messages").appendChild(msgTextdiv);
+    eachMessageDiv.append(div);
+    eachMessageDiv.append(msgTextdiv);
+    document.querySelector(".chat-messages").appendChild(eachMessageDiv);
+    // document.querySelector(".chat-messages").appendChild(msgTextdiv);
   }
 }
 // output db message to dom
@@ -160,19 +148,17 @@ function outputMessage(currentChannelMsg) {
   // `<p class="msgText"> ${currentChannelMsg.message}</p> `
 
   // }else{
-    
+
   div.innerHTML = `<img class="avatar" src="../uploads/${currentChannelMsg.user.profilePic}"></img>`;
-    
+
   msgTextdiv.innerHTML = `<p class="msgUser ">  ${currentChannelMsg.user.name}  <span class= "msgDate">${currentChannelMsg.timestamp} </span></p>
 
     <p class="msgText"> ${currentChannelMsg.message}</p>
     `;
 
-    eachMessageDiv.append(div)
-    eachMessageDiv.append(msgTextdiv)
-    document.querySelector(".chat-messages").appendChild(eachMessageDiv);
+  eachMessageDiv.append(div);
+  eachMessageDiv.append(msgTextdiv);
+  document.querySelector(".chat-messages").appendChild(eachMessageDiv);
   // document.querySelector(".chat-messages").appendChild(msgTextdiv);
   // }
 }
-
-
